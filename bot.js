3618d2b2127d757d5ece90a770471d3c11ce1d06
kingta001 <53026135+kingta001@users.noreply.github.com>
2019-07-21 10:23:56 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const myid = ['596778797551058964'];
+const prefix = ['-'];
+console.log("Scrpit By DesTroyeR");
+
+client.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === prefix+'ارسال'){
+        message.channel.send("#credits"+"<@"+ mymid +">"+"2000")
+    }
+});
+
+client.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+
+client.on("ready", () => {
+let channel =     client.channels.get("602512387781689357")
+setInterval(function() {
+channel.send(`@here @here  https://discord.gg/aecGfZF`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
